b658ca17a2aa6f8b72f1a527c2a5a00cd6344562
lukegjpotter <lukegjpotter@gmail.com>
2016-02-14 00:42:48 +0000

    Properly parsing the HQ Locations

--- src/main/java/com/lukegjpotter/spring/application/service/CsvFileReaderService.java
+++ src/main/java/com/lukegjpotter/spring/application/service/CsvFileReaderService.java
@@ -21,6 +21,7 @@ public class CsvFileReaderService {
 	private static final Logger log = Logger.getLogger(CsvFileReaderService.class.getName());
 
 	private String csvFileLocation;
+	private final String csvDelimiter = ",";
 
 	public List<CyclingEventModel> readRoadRaces() {
 
@@ -30,41 +31,9 @@ public class CsvFileReaderService {
 		try {
 			bufferedReader = new BufferedReader(new FileReader(csvFileLocation));
 			String line = "";
-			final String csvDelimiter = ",";
 
 			while ((line = bufferedReader.readLine()) != null) {
-
-				String[] roadRace = line.split(csvDelimiter);
-
-				String eventName = roadRace[0].trim();
-				String startDate = roadRace[1].trim();
-				String endDate = roadRace[2].trim();
-				String day = roadRace[3].trim();
-				String province = roadRace[4].trim();
-				String classification = roadRace[5].trim();
-				String eventType = roadRace[6].trim();
-				String isAPlus = roadRace[7].trim();
-				String isA1 = roadRace[8].trim();
-				String isA2 = roadRace[9].trim();
-				String isA3 = roadRace[10].trim();
-				String isA4 = roadRace[11].trim();
-				String isVets = roadRace[12].trim();
-				String isWoman = roadRace[13].trim();
-				String isJunior = roadRace[14].trim();
-				Integer isYouth = new Integer(roadRace[15].trim());
-				String primaryContactPerson = roadRace[16].trim();
-				String primaryContactPersonsEmail = roadRace[17].trim();
-				String promotingClub = roadRace[18].trim();
-				String distance = roadRace[19].trim();
-				String hQLocation = roadRace[20].trim();
-				String signOnTime = roadRace[21].trim();
-
-				CyclingEventModel roadRaceModel = new RoadRaceModel(eventName, startDate, endDate, day, province,
-						classification, eventType, isAPlus, isA1, isA2, isA3, isA4, isVets, isWoman, isJunior, isYouth,
-						primaryContactPerson, primaryContactPersonsEmail, promotingClub, distance, hQLocation,
-						signOnTime);
-
-				roadRaces.add(roadRaceModel);
+				roadRaces.add(populateCyclingEventModelFromLine(line));
 			}
 
 		} catch (IOException e) {
@@ -79,6 +48,64 @@ public class CsvFileReaderService {
 		}
 		Collections.sort(roadRaces, new DateOrderedCyclingEventsListComparator());
 		return roadRaces;
+	}
+
+	private CyclingEventModel populateCyclingEventModelFromLine(String line) {
+
+		String[] roadRace = line.split(csvDelimiter);
+
+		String eventName = roadRace[0].trim();
+		String startDate = roadRace[1].trim();
+		String endDate = roadRace[2].trim();
+		String day = roadRace[3].trim();
+		String province = roadRace[4].trim();
+		String classification = roadRace[5].trim();
+		String eventType = roadRace[6].trim();
+		String isAPlus = roadRace[7].trim();
+		String isA1 = roadRace[8].trim();
+		String isA2 = roadRace[9].trim();
+		String isA3 = roadRace[10].trim();
+		String isA4 = roadRace[11].trim();
+		String isVets = roadRace[12].trim();
+		String isWoman = roadRace[13].trim();
+		String isJunior = roadRace[14].trim();
+		Integer isYouth = new Integer(roadRace[15].trim());
+		String primaryContactPerson = roadRace[16].trim();
+		String primaryContactPersonsEmail = roadRace[17].trim();
+		String promotingClub = roadRace[18].trim();
+		String distance = roadRace[19].trim();
+		String hQLocation = handleVariousHqLocationFormats(roadRace);
+		String signOnTime = roadRace[roadRace.length - 1].trim();
+
+		CyclingEventModel roadRaceModel = new RoadRaceModel(eventName, startDate, endDate, day, province,
+				classification, eventType, isAPlus, isA1, isA2, isA3, isA4, isVets, isWoman, isJunior, isYouth,
+				primaryContactPerson, primaryContactPersonsEmail, promotingClub, distance, hQLocation, signOnTime);
+
+		return roadRaceModel;
+	}
+
+	private String handleVariousHqLocationFormats(String[] roadRace) {
+
+		int startPosition = 20;
+		int endPosition = roadRace.length - 2;
+
+		if (startPosition == endPosition)
+			return roadRace[startPosition].trim();
+
+		String hqLocation = "";
+		for (int i = startPosition; i <= endPosition; i++) {
+			hqLocation += roadRace[i].trim() + ", ";
+		}
+
+		// Remove the leading " character
+		if (hqLocation.charAt(0) == '\"')
+			hqLocation = hqLocation.substring(1);
+
+		// Remove the trailing ",<space> characters
+		if (hqLocation.substring(hqLocation.length() - 3).equals("\", "))
+			hqLocation = hqLocation.substring(0, hqLocation.length() - 3);
+
+		return hqLocation;
 	}
 
 	@Value("${csvfile.location}")
--- src/test/java/com/lukegjpotter/spring/application/service/CsvFileReaderServiceTest.java
+++ src/test/java/com/lukegjpotter/spring/application/service/CsvFileReaderServiceTest.java
@@ -1,8 +1,7 @@
 package com.lukegjpotter.spring.application.service;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,25 +12,51 @@ import com.lukegjpotter.spring.application.CyclingIrelandEventsRestServiceApplic
 import com.lukegjpotter.spring.application.model.RoadRaceModel;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { CyclingIrelandEventsRestServiceApplication.class, CsvFileReaderService.class})
+@SpringApplicationConfiguration(classes = { CyclingIrelandEventsRestServiceApplication.class,
+		CsvFileReaderService.class })
 public class CsvFileReaderServiceTest {
 
 	@Autowired
 	CsvFileReaderService csvFileReaderService;
-	RoadRaceModel roadRace;
-	String testCsvFileLocation = "src/test/resources/RoadRacesTest.csv";
-
-	@Before
-	public void setUp() throws Exception {
-		roadRace = new RoadRaceModel("Luke GJ GP", "07/05/2016", "", "Saturday", "Leinster", "Road", "Road", "", "", "",
-				"Yes", "Yes", "", "Yes", "Yes", 0, "Luke Campbell", "lukecampbell1@hotmail.com", "Luke GJ Wheelers",
-				"68.00", "Luke GJ GAA", "9.00");
-		csvFileReaderService.setCsvFileLocation(testCsvFileLocation);
-	}
+	RoadRaceModel expectedRoadRace;
 
 	@Test
 	public void testReadCsvFile() {
-		assertTrue(csvFileReaderService.readRoadRaces().contains(roadRace));
+		expectedRoadRace = new RoadRaceModel("Luke GJ GP", "07/05/2016", "", "Saturday", "Leinster", "Road", "Road", "",
+				"", "", "Yes", "Yes", "", "Yes", "Yes", 0, "Luke Campbell", "lukecampbell1@hotmail.com",
+				"Luke GJ Wheelers", "68.00", "Luke GJ GAA", "9.00");
+		String testCsvFileWithoutCommaInHqLocationLocation = "src/test/resources/RoadRacesTest.csv";
+		csvFileReaderService.setCsvFileLocation(testCsvFileWithoutCommaInHqLocationLocation);
+		assertEquals(expectedRoadRace, csvFileReaderService.readRoadRaces().get(0));
 	}
 
+	@Test
+	public void testReadCsvFileWithCommaInHqLocation() {
+		expectedRoadRace = new RoadRaceModel("Luke GJ GP", "07/05/2016", "", "Saturday", "Leinster", "Road", "Road", "",
+				"", "", "Yes", "Yes", "", "Yes", "Yes", 0, "Luke Campbell", "lukecampbell1@hotmail.com",
+				"Luke GJ Wheelers", "68.00", "Luke GJ, GAA", "9.00");
+		String testCsvFileWithCommaInHqLocationLocation = "src/test/resources/RoadRacesTestCommaInHqLocation.csv";
+		csvFileReaderService.setCsvFileLocation(testCsvFileWithCommaInHqLocationLocation);
+		assertEquals(expectedRoadRace, csvFileReaderService.readRoadRaces().get(0));
+	}
+
+	@Test
+	public void testReadCsvFileWithCommaAndCountyInHqLocation() {
+		expectedRoadRace = new RoadRaceModel("Luke GJ GP", "07/05/2016", "", "Saturday", "Leinster", "Road", "Road", "",
+				"", "", "Yes", "Yes", "", "Yes", "Yes", 0, "Luke Campbell", "lukecampbell1@hotmail.com",
+				"Luke GJ Wheelers", "68.00", "Luke GJ, GAA, Co. Lukin", "9.00");
+		String testCsvFileWithCommaAndCountyInHqLocation = "src/test/resources/RoadRacesTestCommaAndCountyInHqLocation.csv";
+		csvFileReaderService.setCsvFileLocation(testCsvFileWithCommaAndCountyInHqLocation);
+		assertEquals(expectedRoadRace, csvFileReaderService.readRoadRaces().get(0));
+	}
+
+	@Test
+	public void testReadCsvFileWithNorthernIrelandHqLocation() {
+		expectedRoadRace = new RoadRaceModel("Luke GJ GP", "07/05/2016", "", "Saturday", "Leinster", "Road", "Road", "",
+				"", "", "Yes", "Yes", "", "Yes", "Yes", 0, "Luke Campbell", "lukecampbell1@hotmail.com",
+				"Luke GJ Wheelers", "68.00", "Luke GJ, GAA Pitch, Luke Rd., Co. Lukin, BT12 4DB", "9.00");
+		String testCsvFileWithNorthernIrelandHqLocation = "src/test/resources/RoadRacesTestNorthernIrelandHqLocation.csv";
+		csvFileReaderService.setCsvFileLocation(testCsvFileWithNorthernIrelandHqLocation);
+		assertEquals(expectedRoadRace, csvFileReaderService.readRoadRaces().get(0));
+	}
 }
