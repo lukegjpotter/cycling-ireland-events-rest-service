0a933251a2f92a64380f12c622e4d0c7486fed44
lukegjpotter <lukegjpotter@gmail.com>
2018-03-18 23:08:55 +0000

    Issue #3 Using Environment and parsing Password Exception

--- src/main/java/com/lukegjpotter/spring/application/config/DatabaseConfiguration.java
+++ src/main/java/com/lukegjpotter/spring/application/config/DatabaseConfiguration.java
@@ -1,10 +1,12 @@
 package com.lukegjpotter.spring.application.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 import java.net.URI;
@@ -13,13 +15,21 @@ import java.net.URISyntaxException;
 @Configuration
 public class DatabaseConfiguration {
 
+    @Autowired
+    Environment environment;
+
     @Bean
     public DataSource dataSource() throws URISyntaxException {
 
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        URI dbUri = new URI(environment.getProperty("DATABASE_URL", "postgres://postgres:@localhost:5432/cyclingirelandevents"));
 
         String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
+        String password;
+        try {
+            password = dbUri.getUserInfo().split(":")[1];
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            password = "";
+        }
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
         String driverClassName = "org.postgresql.Driver";
 
